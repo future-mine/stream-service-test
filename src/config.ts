4a011b41c06aa0f78e67c7dbad149de7aacae3d8
future-mine <itaverydifficult@outlook.com>
2021-11-09 01:38:01 -0700

    Update host url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
 export const port = process.env.PORT || 5001
-export const host = "https://twitch-stream-test.herokuapp.com"
+export const host =
+    process.env.NODE_ENV === "dev"
+        ? `http://localhost:${port}`
+        : "https://twitch-stream-test.herokuapp.com"
